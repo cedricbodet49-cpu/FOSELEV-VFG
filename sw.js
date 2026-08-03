2d92a0b9a229f7e23791656df44a76ecd364e791
cedricbodet49-cpu <cedric.bodet49@gmail.com>
2026-08-03 15:35:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'foselev-vfg-v3-1-0';
+const CACHE = 'foselev-vfg-v3-1-1';
 const ASSETS = [
   './','./index.html','./style.css','./manifest.webmanifest','./parc.json',
   './parc.js','./js/app-v3.1.js','./icon-192.svg','./icon-512.svg'
